065d3dfe083e6d823a65e45fa7efd38acf97be1d
Aaron Erlandson <wl7521ol@mnstate.edu>
2022-01-20 11:00:08 -0600

    First Commit

---
+++ examples.R
@@ -0,0 +1,21 @@
+# load packages
+
+library(tidyverse)
+library(palmerpenguins)
+penguins
+View(penguins)
+Library(ggplot2)
+library(ggplot2)
+ggplot2(data=penguins)
+ggplot2(data = penguins)
+ggplot(data = penguins)
+view(penguins)
+View(penguins)
+
+ggplot(data = penguins)
+geom_point(body_mass_g)
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+library(tidyverse)
+library(palmerpenguns)
+library(palmerpenguins)
